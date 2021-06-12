e2ed5abecb7d2563313b6d96b3a3a828aa7c4cf2
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2021-06-12 21:07:58 +0200

    Keep header visible if menu is open

--- resources/js/components/menu.js
+++ resources/js/components/menu.js
@@ -3,27 +3,30 @@ export default {
     _menu       : document.querySelector("[data-menu-content]"),
     _header     : document.querySelector("[data-header]"),
 	init() {
-        var lastScrollTop = 0;
-        
-        // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+        let lastScrollTop = 0;
+        let hamburgerButton = document.querySelector("[data-menu-button]");
+        console.log(hamburgerButton);
+
         window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-        if (st > lastScrollTop){
-            // downscroll code
-            document.querySelector("[data-header]").classList.remove("active")
-
-            
-        } else {
-            // upscroll code
-            document.querySelector("[data-header]").classList.add("active");
-
-        }
-        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+            //let activeMenu = this._hamburger.classList.contains("is-active");
+            let hamburgerButtonActive = hamburgerButton.classList.contains("is-active");
+            console.log(hamburgerButtonActive);
+        
+            let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+            if (!hamburgerButtonActive && st > lastScrollTop){
+                // downscroll code
+                document.querySelector("[data-header]").classList.remove("active")
+            } else {
+                // upscroll code
+                document.querySelector("[data-header]").classList.add("active");
+            }
+            lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
         }, false);
 
-        if(this._hamburger && this._menu){
-            this._hamburger.addEventListener("click", () => {
-                this._hamburger.classList.toggle("is-active");
+
+        if(hamburgerButton && this._menu){
+            hamburgerButton.addEventListener("click", () => {
+                hamburgerButton.classList.toggle("is-active");
             })
         }
     }
