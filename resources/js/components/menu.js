a141cd0a6cfb6aabd87ae18a0ec8bad64adeba13
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2021-06-12 13:03:31 +0200

    :sparkles: Add scroll reveal of header

--- resources/js/components/menu.js
+++ resources/js/components/menu.js
@@ -1,8 +1,26 @@
 export default {
     _hamburger  : document.querySelector("[data-menu-button]"),
     _menu       : document.querySelector("[data-menu-content]"),
+    _header     : document.querySelector("[data-header]"),
 	init() {
-        console.log("running");
+        var lastScrollTop = 0;
+        
+        // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+        window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        if (st > lastScrollTop){
+            // downscroll code
+            document.querySelector("[data-header]").classList.remove("active")
+
+            
+        } else {
+            // upscroll code
+            document.querySelector("[data-header]").classList.add("active");
+
+        }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+        }, false);
+
         if(this._hamburger && this._menu){
             this._hamburger.addEventListener("click", () => {
                 this._hamburger.classList.toggle("is-active");
