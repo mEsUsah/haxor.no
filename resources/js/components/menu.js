e9036738ebe1f660b37befbe1f3d938c6dfe01eb
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2021-06-12 21:17:11 +0200

    move chapter navigation down a bit if header is visible

--- resources/js/components/menu.js
+++ resources/js/components/menu.js
@@ -1,30 +1,30 @@
 export default {
-    _hamburger  : document.querySelector("[data-menu-button]"),
-    _menu       : document.querySelector("[data-menu-content]"),
-    _header     : document.querySelector("[data-header]"),
 	init() {
         let lastScrollTop = 0;
         let hamburgerButton = document.querySelector("[data-menu-button]");
-        console.log(hamburgerButton);
+        let sidebar = document.querySelector(".article__content--sidebar");
+        const header = document.querySelector("[data-header]");
+        const menu = document.querySelector("[data-menu-content]");
 
         window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
             //let activeMenu = this._hamburger.classList.contains("is-active");
             let hamburgerButtonActive = hamburgerButton.classList.contains("is-active");
-            console.log(hamburgerButtonActive);
         
             let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
             if (!hamburgerButtonActive && st > lastScrollTop){
                 // downscroll code
-                document.querySelector("[data-header]").classList.remove("active")
+                header.classList.remove("active")
+                sidebar.classList.remove("menuOpen");
             } else {
                 // upscroll code
-                document.querySelector("[data-header]").classList.add("active");
+                header.classList.add("active");
+                sidebar.classList.add("menuOpen");
             }
             lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
         }, false);
 
 
-        if(hamburgerButton && this._menu){
+        if(hamburgerButton && menu){
             hamburgerButton.addEventListener("click", () => {
                 hamburgerButton.classList.toggle("is-active");
             })
