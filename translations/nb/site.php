13d9ff22335328e12881f43ebca31d8b203143ba
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-06-20 21:27:39 +0200

    add translations to course reviews

--- translations/nb/site.php
+++ translations/nb/site.php
@@ -1,12 +1,15 @@
 <?php
 
 return [
-    'Updated' => 'Sist oppdatert',
-    'Content' => 'Innhold',
-    'Search Results' => 'Søkeresultat',
-    'results on' => 'treff på',
-    'Search' => 'Søk',
-    'Completed' => 'Fullført',
-    'Complete' => 'Fullfør',
-    'Submit' => 'Send inn',
+    'Updated'           => 'Sist oppdatert',
+    'Content'           => 'Innhold',
+    'Search Results'    => 'Søkeresultat',
+    'results on'        => 'treff på',
+    'Search'            => 'Søk',
+    'Completed'         => 'Fullført',
+    'Complete'          => 'Fullfør',
+    'Submit'            => 'Send inn',
+    'Review'            => 'Anmeldelse',
+    'and certificate'   => 'og kursbevis',
+    'and result'        => 'og resultat'
 ];
