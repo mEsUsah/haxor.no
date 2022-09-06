37a0660b75dddb9bd472bcf2dbe49210320afbca
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-09-06 21:27:51 +0200

    update site plugin to handle craft 4

--- plugins/haxor/src/Haxor.php
+++ plugins/haxor/src/Haxor.php
@@ -64,21 +64,21 @@ class Haxor extends Plugin
      *
      * @var string
      */
-    public $schemaVersion = '1.0.0';
+    public string $schemaVersion = '1.0.0';
 
     /**
      * Set to `true` if the plugin should have a settings view in the control panel.
      *
      * @var bool
      */
-    public $hasCpSettings = false;
+    public bool $hasCpSettings = false;
 
     /**
      * Set to `true` if the plugin should have its own section (main nav item) in the control panel.
      *
      * @var bool
      */
-    public $hasCpSection = false;
+    public bool $hasCpSection = false;
 
     // Public Methods
     // =========================================================================
