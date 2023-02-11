e546dee4a38ba52e394e4b882d892c10604a354a
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-02-11 17:13:32 +0000

    switch to laravel-mix as frontend compiler

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,14 +1,33 @@
-import components from './components/*.js';
+// TODO: Fix dynamic loading of components
 
-const app = {
-    init() {
-        components.forEach((component) => {
-            if (typeof component === 'object' && component.hasOwnProperty('default')) {
-                component = component.default;
-                component.hasOwnProperty('init') ? component.init() : null;
-            }
-        });
-    }
-};
+import accordian from "./components/accordian";
+accordian.init();
+import chapterHighlight from "./components/chapterHighlight";
+chapterHighlight.init();
+import chart from "./components/chart";
+chart.init();
+import lessonTasks from "./components/lessonTasks";
+lessonTasks.init();
+import menu from "./components/menu";
+menu.init();
+import thmList from "./components/thmList";
+thmList.init();
 
-app.init();
+/**
+ * This is the main entry point for webpack.
+ * This file as an "autoloader" for any files placed under the "components/" directory
+ * so just create the directory, place any component file in it, and it should be auto bundled.
+ * The component file "can" have a default export as an object with a method init() that will be called on page load
+ */
+
+// const app = {
+//     init() {
+//         for (let i in components) {
+//             let component = components[i];
+//             if (typeof component === 'object' && component.hasOwnProperty('default')) {
+//                 component.default.hasOwnProperty('init') ? component.default.init() : null;
+//             }
+//         }
+//     }
+// };
+// app.init();
