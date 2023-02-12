3009884fda28112844ffa547b558e22e4257dad2
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-02-12 11:14:21 +0000

    add translations

--- translations/nb/site.php
+++ translations/nb/site.php
@@ -1,16 +1,18 @@
 <?php
 
 return [
-    'Updated'           => 'Sist oppdatert',
-    'Content'           => 'Innhold',
-    'Search Results'    => 'Søkeresultat',
-    'results on'        => 'treff på',
-    'Search'            => 'Søk',
-    'Completed'         => 'Fullført',
-    'Complete'          => 'Fullfør',
-    'Submit'            => 'Send inn',
-    'Review'            => 'Anmeldelse',
-    'Description'       => 'Beskrivelse',
-    'and certificate'   => 'og kursbevis',
-    'and result'        => 'og resultat'
+    'Updated'                               => 'Sist oppdatert',
+    'Content'                               => 'Innhold',
+    'Search Results'                        => 'Søkeresultat',
+    'results on'                            => 'treff på',
+    'Search'                                => 'Søk',
+    'Completed'                             => 'Fullført',
+    'Complete'                              => 'Fullfør',
+    'Submit'                                => 'Send inn',
+    'Review'                                => 'Anmeldelse',
+    'Description'                           => 'Beskrivelse',
+    'and certificate'                       => 'og kursbevis',
+    'and result'                            => 'og resultat',
+    'List of all courses I have reviewed'   => 'Oversikt over alle kurs jeg har anmeldt',
+    'List of all books I have reviewed'   => 'Oversikt over alle bøker jeg har anmeldt',
 ];
