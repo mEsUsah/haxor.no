82c96170b106059e7a8579c30ef319ebc4f6d0f1
Stanley Skarshaug <stanley@haxor.no>
2024-09-29 15:38:14 +0200

    fix: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+PROD_HOST="c1.haxor.no"
+PROD_USER="stanley"
+PROJECT_ROOT="/var/www/www.haxor.no"
+
+if [[ `hostname` == $PROD_HOST && `whoami` == $PROD_USER && `pwd` == $PROJECT_ROOT ]]; then
+    git pull origin prod
+    sudo chown -R www-data:www-data .
+    sudo -u www-data php8.2 /usr/bin/composer install
+    sudo -u www-data php8.2 craft project-config/apply
+else
+    echo "Error: Incorrect host, user, or directory."
+fi
