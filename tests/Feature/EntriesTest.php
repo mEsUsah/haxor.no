d8ead9dd6c2f1865dfcd1341c431ad6c2df53c1e
Stanley Skarshaug <stanley@haxor.no>
2024-09-29 17:13:52 +0200

    test: rename ArticlesTest to EntriesTest, and add more sections

---
+++ tests/Feature/EntriesTest.php
@@ -0,0 +1,41 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use craft\elements\Entry;
+use craft\helpers\App;
+use GuzzleHttp\Client;
+
+final class EntriesTest extends TestCase
+{    
+    public function test_can_view_all_published(): void
+    {
+        $client = new Client([
+            'base_uri' =>  App::env('PRIMARY_SITE_URL'),
+            'timeout'  => 4.0,
+        ]);
+        
+        $entries = Entry::find()
+            ->section([
+                // Singles 
+                "home",         // Home
+                "aboutme",      // About Me
+                "aboutblog",    // About blog
+                "ctfWriteups",  // CTF Writeup (Overview)
+                "bookReviews",  // Book reviews (Overview)
+                "course",       // My courses (Overview)
+                
+                // Channels with viewable content
+                "articles",     // Articles
+                "portfolio",    // Portfolio
+                "lesson",       // Lessons
+                "ctfWriteup",   // CTF Writeup
+            ])
+            ->all();
+
+        foreach ($entries as $entry) {
+            $route = $entry->getUrl();
+            $response = $client->request('GET', $route);
+            
+            $this->assertEquals(200, $response->getStatusCode());
+        }
+    }
+}
