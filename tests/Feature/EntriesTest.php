c8df88189d85e0c6b8c5a289662be6d2c84bb2f7
Stanley Skarshaug <stanley@haxor.no>
2024-12-10 11:50:02 +0100

    test: replace article test with the new dynamic one

--- tests/Feature/EntriesTest.php
+++ tests/Feature/EntriesTest.php
@@ -1,46 +1,58 @@
 <?php
 namespace Tests\Feature;
 
+use Craft;
 use PHPUnit\Framework\TestCase;
 use craft\elements\Entry;
 use craft\helpers\App;
 use GuzzleHttp\Client;
-use Tests\utils\ClearCache;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
 
 final class EntriesTest extends TestCase
 {    
     public function test_can_view_all_published(): void
     {
-        ClearCache::run();
-        
         $client = new Client([
             'base_uri' =>  App::env('PRIMARY_SITE_URL'),
-            'timeout'  => 4.0,
+            'timeout'  => 10.0,
+            'verify' => false
         ]);
-        
-        $entries = Entry::find()
-            ->section([
-                // Singles 
-                "home",         // Home
-                "aboutme",      // About Me
-                "aboutblog",    // About blog
-                "ctfWriteups",  // CTF Writeup (Overview)
-                "bookReviews",  // Book reviews (Overview)
-                "course",       // My courses (Overview)
-                
-                // Channels with viewable content
-                "articles",     // Articles
-                "portfolio",    // Portfolio
-                "lesson",       // Lessons
-                "ctfWriteup",   // CTF Writeup
-            ])
-            ->all();
 
-        foreach ($entries as $entry) {
-            $route = $entry->getUrl();
-            $response = $client->request('GET', $route);
-            
-            $this->assertEquals(200, $response->getStatusCode());
+        $sites = Craft::$app->getSites()->getAllSites();
+        $entries = [];
+        
+        // Loop through all sites and get all entries
+        foreach ($sites as $site) {
+            if($site->handle == 'default') {
+                continue;
+            }
+
+            $siteEntries = Entry::find()->site($site->handle)->all();
+            array_push($entries, ...$siteEntries);
         }
+
+        // Loop through all entries and create a request for each
+        $requests = function () use ($entries) {
+            foreach ($entries as $entry) {
+                if($entry->getUrl() == null) {
+                    continue;
+                }
+                yield new Request('HEAD', $entry->getUrl());
+            }
+        };
+
+        // Send all requests concurrently
+        $pool = new Pool($client, $requests(), [
+            'concurrency' => 20,  // Adjust this number based on your server's capacity
+            'fulfilled' => function ($response, $index) {
+                $this->assertEquals(200, $response->getStatusCode());
+            },
+            'rejected' => function ($exception, $index) {
+                $this->fail("Request failed: " . $exception->getMessage());
+            },
+        ]);
+
+        $pool->promise()->wait();
     }
 }
