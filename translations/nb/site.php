b3f4c174a4f78ba4d8ab8d89759ff89098c4976f
Stanley Skarshaug <stanley@haxor.no>
2024-12-15 13:04:43 +0100

    fix(footer): translate texts

--- translations/nb/site.php
+++ translations/nb/site.php
@@ -14,5 +14,11 @@ return [
     'and certificate'                       => 'og kursbevis',
     'and result'                            => 'og resultat',
     'List of all courses I have reviewed'   => 'Oversikt over alle kurs jeg har anmeldt',
-    'List of all books I have reviewed'   => 'Oversikt over alle bøker jeg har anmeldt',
+    'List of all books I have reviewed'     => 'Oversikt over alle bøker jeg har anmeldt',
+    'Read more about the hacker emblem'     => 'Les mer om hacker emblemet',
+    'Read more about the not by AI emblem'  => 'Les mer om ikke-av-AI emblemet',
+    'Go to my linkedin account'             => 'Besøk min linkedin profil',
+    'Go to my github account'               => 'Besøk min github profil',
+    'Icons by'                              => 'Ikoner fra',
+    'and'                                   => 'og'
 ];
